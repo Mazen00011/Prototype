09a71feb0f0604ca4ef1c62e61cc17d76f1b4024
Mazen00011 <mazenlito@outlook.com>
2025-09-07 08:19:21 +0300

    script.js

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ window.addEventListener('load', () => {
 });
 
 function showorhidemenu() {
+    const mainNav = document.getElementById("mainnav");
     const menu = document.getElementById("mobilenav");
     menu.classList.toggle("show");
+
+    if (menu.classList.contains("show")) {
+        mainNav.classList.add("menuToggled");
+    } else {
+        mainNav.classList.remove("menuToggled");
+    }
 }
